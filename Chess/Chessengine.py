424e169787e2faa90b455e669961554a84654ce5
DatTwinkie <pawelsitko90@gmail.com>
2023-03-12 21:04:00 +0000

    Commit 5 (Complete Chess Board and Piece movement)

--- Chess/Chessengine.py
+++ Chess/Chessengine.py
@@ -13,7 +13,51 @@ class GameState():
             ["--","--","--","--","--","--","--","--"],
             ["WP","WP","WP","WP","WP","WP","WP","WP"],
             ["WR","WN","WB","WQ","WK","WB","WN","WR"]]
-        #tacks whoevers turn it is
+        #tracks whoevers turn it is
         self.whiteMove = True
         #keeps track of the games special moves
         self.Move_Log =[]
+
+    def moveMaking(self, MovePiece):
+        # Sets the starting square of the moved piece to an empty square
+        self.board[MovePiece.startingRow][MovePiece.startingColumn] = "--"
+        # Sets the ending square of the moved piece to the moved piece
+        self.board[MovePiece.EndingRow][MovePiece.EndingColumn] = MovePiece.movedPiece
+        # Adds the move to the Move_Log so it can be undone later
+        self.Move_Log.append(MovePiece)
+        self.Move_Log.append(MovePiece)
+        # Toggles the value of self.whiteMove to swap the player
+        self.whiteMove = not self.whiteMove
+
+
+class Chess_Move():
+    # Maps row numbers to row numbers
+    ranks_to_row = {"1": 7, "2": 6, "3": 5, "4": 4,
+                    "5": 3, "6": 2, "7": 1, "8": 0}
+    # Maps row numbers to row numbers
+    rows_to_ranks = {v: k for k, v in ranks_to_row.items()}
+    # Maps file letters to column numbers
+    file_to_column = {"a": 0, "b": 1, "c": 2, "d": 3,
+                     "e": 4, "f": 5, "g": 6, "h": 7}
+    # Maps index numbers to file lettersx
+    column_to_files = {v: k for k, v in file_to_column.items()}
+
+    def __init__(self, Starting_Square, Ending_Square, board):
+        # Extract the starting row and column from the starting square
+        self.startingRow = Starting_Square[0]
+        self.startingColumn = Starting_Square[1]
+        # Extract the ending row and column from the ending square
+        self.EndingRow = Ending_Square[0]
+        self.EndingColumn = Ending_Square[1]
+        # Store the piece that was moved
+        self.movedPiece = board[self.startingRow][self.startingColumn]
+        # Store the piece that was captured (if any)
+        self.capturedPiece = board[self.EndingRow][self.EndingColumn]
+
+    def create_Chess_Notation(self):
+        # Returns the starting square and ending square in chess notation
+        return self.create_Rank_File(self.startingRow, self.startingColumn) + self.create_Rank_File(self.EndingRow, self.EndingColumn)
+
+    def create_Rank_File(self, Row, Column):
+        # Converts row and column indices to a chess square
+        return self.column_to_files[Column] + self.rows_to_ranks[Row]
--- Chess/Chessmain.py
+++ Chess/Chessmain.py
@@ -16,9 +16,10 @@ Load_Images = {}
 #initialise a global dictionary of images. this will be called once in the main"
 
 def startImages():
-    pieces = ["WhiteRook","WhiteKnight","WhiteBishop","WhiteKing","WhiteQueen","WhitePawn","BlackRook","BlackKnight","BlackBishop","BlackKing","BlackQueen","BlackPawn"]
-    for piece in pieces:
-        Load_Images[piece] = pygame.transform.scale(pygame.image.load("/Users/pawel/PycharmProjects/ChessGame/Chess/Chess images/" + piece + ".png"), (Square_size, Square_size))
+    pieces = ["WR","WN","WB","WK","WQ","WP","BR","BN","BB","BK","BQ","BP"]
+    for chess_piece in pieces:
+        Load_Images[chess_piece] = pygame.transform.scale(pygame.image.load("/Users/pawel/PycharmProjects/ChessGame/Chess/Chess images/" + chess_piece + ".png"), (float(Square_size*0.97), float(Square_size*0.97)))
+
     #allows me to access an image by saying 'images['WhiteRook']'
 
     #handle user input and graphics
@@ -32,11 +33,47 @@ def main():
     # Initialize the game state and load images
     startImages()
     infiniteloop = True
+    # holds which square is clicked on
+    square_Select = ()
+    user_Click = []
+
+    # main game loop
     while infiniteloop:
-        for x in pygame.event.get():
-            if x.type == pygame.QUIT:
+        for event in pygame.event.get():
+            # checks if user clicked on quit button
+            if event.type == pygame.QUIT:
                 infiniteloop = False
                 main_menu_screen = pygame.display.set_mode((1280, 720))
+            # checks if user clicked on the board
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # gets the location of the mouse click
+                mouse_location = pygame.mouse.get_pos()
+                # calculates which row and column the user clicked on
+                column = mouse_location[0] // Square_size
+                row = mouse_location[1] // Square_size
+
+                # if user clicked on the same square again
+                if square_Select == (row, column):
+                    # deselect the square
+                    square_Select = ()
+                    user_Click = []
+                else:
+                    # select the square and add it to user_Click
+                    square_Select = (row, column)
+                    user_Click.append(square_Select)
+
+                # if the user has selected two squares
+                if len(user_Click) == 2:
+                    # Create a Chess_Move object for the player's move
+                    player_move = Chessengine.Chess_Move(user_Click[0], user_Click[1],gamestate.board)
+                    # Print the notation of the move that was made
+                    print(player_move.create_Chess_Notation())
+                    # Make the move on the gamestate board
+                    gamestate.moveMaking(player_move)
+                    # Reset the selected square
+                    square_Select = ()
+                    # Reset the user's click
+                    user_Click = []
 
         creategamestate(displaysize, gamestate)
         game_clock.tick(Fps_Limit)
@@ -47,24 +84,30 @@ def creategamestate(displaysize, gamestate):
     createPieces(displaysize, gamestate.board)
 # Function to create the chess board on the screen
 def createBoard(displaysize):
-    #makes a darker green throug the RGB values (Red green Blue)
+    #makes a darker green through the RGB values (Red green Blue)
     darker_green = pygame.Color(0, 100, 0)
     # Define the colours of the squares
     colours = [pygame.Color("White"), darker_green]
 
     # Loop through each row and column of the board
-    for r in range(Dimension_Screen):
-        for c in range(Dimension_Screen):
+    for row in range(Dimension_Screen):
+        for column in range(Dimension_Screen):
             # Determine the colour of the square based on its position on the board, which uses coordinates
-            colour = colours[((r + c) % 2)]
+            colour = colours[((row + column) % 2)]
 
             # Draws a rectangle on the display with the appropriate colour and size
             # The rectangle's position and size are based on the row and column and the size of the squares
-            pygame.draw.rect(displaysize, colour, pygame.Rect(c * Square_size, r * Square_size, Square_size, Square_size))
+            pygame.draw.rect(displaysize, colour, pygame.Rect(column * Square_size, row * Square_size, Square_size, Square_size))
 
 
 # Function to create the chess pieces on the screen
 def createPieces(displaysize, board):
-    pass
-
-
+    # Loop through each row and column of the board
+    for row in range(Dimension_Screen):
+        for column in range(Dimension_Screen):
+            # Get the chess piece at the current row and column
+            chess_piece = board[row][column]
+            # If there is a piece at this location on the board
+            if chess_piece != "--":
+                # Draw the image of the chess piece on the display surface at the appropriate location and size
+                displaysize.blit(Load_Images[chess_piece], pygame.Rect(column*Square_size, row*Square_size, Square_size, Square_size))
